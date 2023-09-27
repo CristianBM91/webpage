1eccc096c2b6b8d1c11ce9791cdee4da6c3c1fc8
Cristian <cristianbm91@gmail.com>
2023-09-27 10:55:03 +0200

    comentar logica fake

--- scripts/leerUltimaMedicion.js
+++ scripts/leerUltimaMedicion.js
@@ -43,12 +43,9 @@ const IP_PUERTO="http://localhost:1234";
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Agregar un evento click al botón
     var boton = document.getElementById('miBoton');
     boton.addEventListener('click', function () {
-        // Coloca aquí el código que deseas ejecutar cuando se presione el botón
-		leerUltimaMedicion()
-        
+		leerUltimaMedicion() 
     });
     function leerUltimaMedicion( datos, cb ) {
 
@@ -57,9 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
             var pResultadoFecha = document.getElementById('resultadoFecha');
             var pResultadoLocalizacion = document.getElementById('resultadoLocaliz');
 
-        // Verifica si se encontró el elemento
         if (pResultado) {
-            // Actualiza el contenido de texto del elemento h2
             objetoJson = resultado.slice(1, -1);
             objetoJson = JSON.parse(objetoJson);
             pResultado.textContent = objetoJson.valorOzono + " ppm";
@@ -68,16 +63,11 @@ document.addEventListener('DOMContentLoaded', function () {
             pResultadoLocalizacion.textContent = objetoJson.localiz;
            
            
-        // Obtener el elemento p por su id
         var miParrafo = document.getElementById('resultadoOzono');
         var miFondo = document.getElementById('textoOzono');
-       
-
-      
-        // Obtener el valor del elemento p y convertirlo a un número
         var valor = parseInt(miParrafo.textContent);
         console.log(valor);
-        // Verificar si el valor es mayor que 500 y aplicar la clase CSS correspondiente
+       
         if (valor < 200) {
             miFondo.style.backgroundColor = 'green';
             
@@ -97,22 +87,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         } )
         
-    } // ()
+    } // leerUltimaMedicion()
 
 
     function convertirFechaLegible(cadenaFecha) {
-        // Crea un objeto Date a partir de la cadena de fecha
         var fecha = new Date(cadenaFecha);
       
-        // Obtiene los componentes de la fecha
         var año = fecha.getFullYear();
-        var mes = ("0" + (fecha.getMonth() + 1)).slice(-2); // Agrega 1 al mes ya que enero es 0
+        var mes = ("0" + (fecha.getMonth() + 1)).slice(-2);
         var dia = ("0" + fecha.getDate()).slice(-2);
         var horas = ("0" + fecha.getHours()).slice(-2);
         var minutos = ("0" + fecha.getMinutes()).slice(-2);
         var segundos = ("0" + fecha.getSeconds()).slice(-2);
       
-        // Formatea la fecha en un formato legible
         var fechaLegible = año + "-" + mes + "-" + dia + " " + horas + ":" + minutos + ":" + segundos;
       
         return fechaLegible;
